299f3a09d041fd6355f3014e7d00a4c7e7d8179d
PieterDr <pieter.drijkoningen@vrt.be>
2018-09-12 09:12:19 +0200

    Add threads exercises

---
+++ src/main/java/com/axxes/traineeship/threading/exercise/Threads.java
@@ -0,0 +1,17 @@
+package com.axxes.traineeship.threading.exercise;
+
+public class Threads {
+
+    // Creating and joining threads
+    //
+    // Write a short program that prints "Hello world" from a thread.
+    //
+    // Now modify the program to print "Hello world" five times, once from each
+    // of five different threads.
+    //
+    // Now modify the printed string to include the thread number; ensure that
+    // all threads have a unique thread number.
+    public static void main(String[] args) {
+
+    }
+}
