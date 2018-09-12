3662dbcf8a2e006ffc7619c434ba7b3d28dac004
PieterDr <pieter.drijkoningen@vrt.be>
2018-09-12 14:31:43 +0200

    ExecutorService exercises

---
+++ src/main/java/com/axxes/traineeship/threading/exercise/Executor.java
@@ -0,0 +1,23 @@
+package com.axxes.traineeship.threading.exercise;
+
+public class Executor {
+
+    static class ExecutorServiceExercise {
+
+        // Write a program that submits some tasks to an ExecutorService
+        //
+        // Also submit some tasks that return a value
+        // print the value when it becomes available
+        public static void main(String[] args) {
+
+        }
+    }
+
+    static class CompletionServiceExercise {
+
+        // Use a CompletionService to retrieve the values of the submitted tasks
+        public static void main(String[] args) {
+
+        }
+    }
+}
