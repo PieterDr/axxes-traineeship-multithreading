dc252b4b92e867aa5ec9cabdac5cdb51771c329d
PieterDr <pieter.drijkoningen@vrt.be>
2019-09-04 00:48:19 +0200

    Remove implementation

--- src/main/java/com/axxes/traineeship/threading/exercise/Threads.java
+++ src/main/java/com/axxes/traineeship/threading/exercise/Threads.java
@@ -1,7 +1,5 @@
 package com.axxes.traineeship.threading.exercise;
 
-import java.util.concurrent.ThreadPoolExecutor;
-
 public class Threads {
 
     // Creating and joining threads
@@ -11,24 +9,8 @@ public class Threads {
     // Now modify the program to print "Hello world" five times, once from each
     // of five different threads.
     //
-    // Now modify the printed string to include the thread number; ensure that
-    // all threads have a unique thread number.
+    // Now modify the printed string to include the thread name
     public static void main(String[] args) throws InterruptedException {
-        MyThread t1 = new MyThread();
-        t1.start();
-        t1.join();
-        System.out.println("TEST");
-    }
-
-    static class MyThread extends Thread {
-        @Override
-        public void run() {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println(Thread.currentThread().getName());
-        }
+        //TODO implement
     }
 }
