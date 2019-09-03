25501b59c1495c62de10db27185223638d85ea96
PieterDr <pieter.drijkoningen@vrt.be>
2019-09-04 01:22:50 +0200

    Remove semaphores exercise

--- src/main/java/com/axxes/traineeship/threading/exercise/Semaphores.java
+++
@@ -1,9 +0,0 @@
-package com.axxes.traineeship.threading.exercise;
-
-public class Semaphores {
-
-    // Create a pool of resources that is guarded by a semaphore
-    public static void main(String[] args) {
-
-    }
-}
