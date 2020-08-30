4ef683e5208099cefe0705e480db8043a6147f31
PieterDr <pieter.drijkoningen@vrt.be>
2020-08-31 00:26:17 +0200

    ExerciseThreads solution

--- src/main/java/com/axxes/traineeship/threading/intro/ExerciseThreads.java
+++ src/main/java/com/axxes/traineeship/threading/intro/ExerciseThreads.java
@@ -16,7 +16,16 @@ public class ExerciseThreads {
      *    - extend the Thread class
      */
     private static void creatingThreads() {
-        // TODO implement
+        new Thread(() -> System.out.println(Thread.currentThread().getName() + ": Hello world!")).start();
+        new HelloWorld().start();
+    }
+
+    private static class HelloWorld extends Thread {
+
+        @Override
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + ": Hello world!");
+        }
     }
 
     /*
@@ -25,7 +34,14 @@ public class ExerciseThreads {
      * Write a program that spawns a new Thread that prints the numbers 1...100
      * When that thread is done, print 'Program finished'
      */
-    private static void joiningThreads() {
-        // TODO implement
+    private static void joiningThreads() throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            for (int i = 1; i <= 100; i++) {
+                System.out.println(i);
+            }
+        });
+        thread.start();
+        thread.join();
+        System.out.println("Program finished");
     }
 }
