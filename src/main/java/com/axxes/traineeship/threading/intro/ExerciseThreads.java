af22f45552112e2237ff7e527d65bc50141c25d4
PieterDr <pieter.drijkoningen@vrt.be>
2020-08-31 00:14:45 +0200

    ExerciseThreads

---
+++ src/main/java/com/axxes/traineeship/threading/intro/ExerciseThreads.java
@@ -0,0 +1,31 @@
+package com.axxes.traineeship.threading.intro;
+
+public class ExerciseThreads {
+
+    public static void main(String[] args) throws InterruptedException {
+        creatingThreads();
+        joiningThreads();
+    }
+
+    /*
+     * Creating Threads
+     *
+     * Write a program that spawns a new Thread that prints 'Hello world!' preceded by the name of the thread
+     * Do this in 2 ways:
+     *    - pass the behaviour to the Thread constructor
+     *    - extend the Thread class
+     */
+    private static void creatingThreads() {
+        // TODO implement
+    }
+
+    /*
+     * Joining threads
+     * <p>
+     * Write a program that spawns a new Thread that prints the numbers 1...100
+     * When that thread is done, print 'Program finished'
+     */
+    private static void joiningThreads() {
+        // TODO implement
+    }
+}
