55e24820b6a75c7237c1eb809bfdec3b0797b535
PieterDr <pieter.drijkoningen@vrt.be>
2020-08-31 10:51:22 +0200

    ExerciseExecutors

---
+++ src/main/java/com/axxes/traineeship/threading/executors/ExerciseExecutors.java
@@ -0,0 +1,37 @@
+package com.axxes.traineeship.threading.executors;
+
+public class ExerciseExecutors {
+
+    static class ExecutorServiceExercise {
+
+        /*
+         * Write a program that submits some tasks to an ExecutorService.
+         * Also submit some tasks that return a value. Print the value when it becomes available.
+         */
+        public static void main(String[] args) {
+            // TODO implement
+        }
+    }
+
+    static class CompletionServiceExercise {
+
+        /*
+         * Again create some tasks that return a value.
+         * This time make use of a CompletionService to retrieve the values.
+         */
+        public static void main(String[] args) {
+            // TODO implement
+        }
+    }
+
+    static class TaskLifecycleHooks {
+
+        /*
+         * Extend the ThreadPoolExecutor class to create an executor that times
+         * and prints the duration of submitted tasks.
+         */
+        public static void main(String[] args) {
+            // TODO implement
+        }
+    }
+}
