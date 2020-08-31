e95a4cf66fbe160b2c7a0702941554ce4006c8c9
PieterDr <pieter.drijkoningen@vrt.be>
2020-08-31 03:42:58 +0200

    ExerciseLatches + ExerciseQueues

---
+++ src/main/java/com/axxes/traineeship/threading/buildingblocks/ExerciseLatches.java
@@ -0,0 +1,16 @@
+package com.axxes.traineeship.threading.buildingblocks;
+
+public class ExerciseLatches {
+
+    private static Object resource = null;
+
+    /*
+     * Create a task in a thread that is dependent on the non-initialized resource.
+     * Start the task and then initialize the resource (simulate that this takes some time).
+     *
+     * Use a CountDownLatch to ensure the task only starts when the resource has been initialized.
+     */
+    public static void main(String[] args) {
+        // TODO implement
+    }
+}
